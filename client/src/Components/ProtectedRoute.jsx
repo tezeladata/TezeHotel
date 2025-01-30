58346200917b603a638faf2ebcf7b2b9f1730634
tezeladata <David.tezela@gmail.com>
2025-01-31 02:38:48 +0400

    Added protected route and it's functional will be finished in next version

---
+++ client/src/Components/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+// import { Navigate } from "react-router";
+
+// const ProtectedRoute = ({ children }) => {
+//     const isAdmin = true;
+
+//     return isAdmin ? children : <Navigate to={"/"} />
+// } 
+
+// export default ProtectedRoute;
